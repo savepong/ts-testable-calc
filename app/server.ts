180502244a9370f61aa544ef93d14d204cca774f
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-06-13 10:29:51 +0700

    Add sample upload

--- app/server.ts
+++ app/server.ts
@@ -1,14 +1,22 @@
 import * as Express from 'express'
 import Config from './config'
 import * as BodyParser from 'body-parser'
+import * as Fs from 'fs'
+import * as Multer from 'multer'
+
+const upload = Multer()
 
 const app = Express()
 app.use(BodyParser.json())
 const port = Config.port
 
 app.get('/', (req, res) => res.send('Hello world'))
+app.post('/upload', upload.single('photo'), (req, res) => {
+  Fs.writeFileSync('/tmp/this_is_the_file.png', req.file.buffer)
+  res.send('Succeed')
+})
 
-if (Config.NODE_ENV !== 'TEST')  {
+if (Config.NODE_ENV !== 'TEST') {
   app.listen(port, () => console.log(`App listen to port ${Config.port}`))
 }
 
